5efea43b0a45035c84ec67aeba7a69c57d6e37ea
Kalen <kalen7@gmail.com>
2017-09-02 11:08:07 -0400

    Two-way binding for user to input Hero name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,16 @@ export class Hero {
 
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`,
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `,
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Gallery of Heroes';
